82ec4821b0655ca73d39cb568cc766fe41a8b576
PiHan Hsu <pihanhsu@gmail.com>
2016-04-19 14:49:43 +0800

    modify Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, 'myscoreboard'
 set :repo_url, 'git@github.com:PiHanHsu/myscoreboard-web.git'
 set :deploy_to, '/home/deploy/myscoreboard'
 set :keep_releases, 5
-set :linked_files, fetch(:linked_files, []).push('config/database.yml' 'config/secrets.yml')  # 如果有 facebook.yml 或 email.yml 想要連結的話，也要加進來
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')  # 如果有 facebook.yml 或 email.yml 想要連結的話，也要加進來
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :passenger_restart_with_touch, true
 
