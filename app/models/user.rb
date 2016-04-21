0b0fa9a40925723a0f1e331ece4dacd6e55de5c7
stevenchentw <sadz9999@hotmail.com>
2016-04-21 11:32:36 +0800

    facebook singup -2

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 
       def self.get_fb_data(access_token)
 
-      res = RestClient.get "https://graph.facebook.com/v2.4/me",  { :params => { :access_token => access_token } }
+      res = RestClient.get "https://graph.facebook.com/v2.4/me",  { :params => { :fields => "email,name", :access_token => access_token } }
 
         if res.code == 200
           JSON.parse( res.to_str )
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
        user = User.new
        user.fb_uid = auth.uid
        user.fb_token = auth.credentials.token
-       user.email = auth.info.email
+       user.email = auth[:info][:email]
        user.password = Devise.friendly_token[0,20]
        #user.fb_raw_data = auth
        user.save!
