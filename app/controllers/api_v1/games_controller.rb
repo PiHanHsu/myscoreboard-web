9f9c34729d89ce6f863160a9c06942cc7cd62eff
Damon Liao <damon09273@gmail.com>
2016-04-25 10:00:42 +0800

    Games API：Modified inherit to API controller

--- app/controllers/api_v1/games_controller.rb
+++ app/controllers/api_v1/games_controller.rb
@@ -1,19 +1,19 @@
-class ApiV1::GamesController < ApplicationController
+class ApiV1::GamesController < ApiController
     # before_action :authenticate_user!
     protect_from_forgery except: :create
 	def index
 		games = Game.where( :team_id => params[:team_id])
-   	  
+
    	    render :json => { :message => "Test"}
     end
 
   	def create
 
-    	transaction = Game.transaction do 
+    	transaction = Game.transaction do
  			game = Game.create( :team_id => params[:team_id])
 
     		params[:scores].each do |key, value|
-   
+
     			game.records.create(:game_id => game, :user_id => value["user"], :score => value["score"], :result => value["result"])
 	    	end
     	end
