422de5f0865a3750f6c5c411954d80e7272501ce
Damon Liao <damon09273@gmail.com>
2016-04-26 17:12:32 +0800

    Teams API：Modified Create action

--- app/controllers/api_v1/teams_controller.rb
+++ app/controllers/api_v1/teams_controller.rb
@@ -1,30 +1,38 @@
 class ApiV1::TeamsController < ApiController
   protect_from_forgery :except => [:create, :update]
-  # before_action :test_set_user
-  # before_action :set_user
   before_action :authenticate_user!
   before_action :set_team_params, :only => [:update]
 
   def index
-    @teams = current_user.teams.all
+    @teams = current_user.teams.all.page(params[:page])
   end
 
-
   def create
-    @team = current_user.teams.new( :name => params[:name],
-                             :day => params[:day],
-                             :start_time => params[:start_time],
-                             :end_time => params[:end_time],
-                             :logo => params[:logo]
-                             )
+    # @location = Location.find_or_create_by(:place_name => params[:place_name])
+    @location = Location.find_or_create_by(:place_name => params[:place_name],
+                                           :address => params[:address],
+                                           :lat => params[:lat],
+                                           :lng => params[:lng],
+    # params[:team][:location_id] = @location.id
+
+    @team = current_user.teams.new(:name => params[:name],
+                                   :day => params[:day],
+                                   :start_time => params[:start_time],
+                                   :end_time => params[:end_time],
+                                   :logo => params[:logo])
+    @team.location_id = @location.id
+
+
     if @team.save
       render json: {
-      message: "儲存成功",
-      status: 200  }
+        message: "儲存成功",
+        status: 200
+      }
     else
       render json: {
-      message: "儲存失敗",
-      status: 400  }
+        message: "儲存失敗",
+        status: 400
+      }
     end
   end
 
@@ -36,6 +44,12 @@ class ApiV1::TeamsController < ApiController
                          :end_time => params[:end_time],
                          :logo => params[:logo]
                           )
+    #  params[:user_ids] #=> [1,2,3,...]
+     params[:user_ids][:added].each do |id|
+       UserTeamship.create(:user_id => id, :team_id => @team)
+     end
+     UserTeamship.where(:id => params[:user_ids][:removed]).destroy_all
+     #  UserTeamships.create(:user_id => params[:user_ids], :team_id => @team)
      render json: {
      message: "更新成功",
      status: 200  }
@@ -43,18 +57,19 @@ class ApiV1::TeamsController < ApiController
   end
 
 
-
-
   private
 
-  # def set_user
-  #   @user = User.first
-  # end
 
   def set_team_params
     @team = current_user.teams.find( params[:id] )
   end
 
-
+  # def team_params
+  #   params.require(:team).permit(:name, :day, :start_time, :end_time, :logo)
+  # end
+  #
+  # def location_params
+  #   params.require(:location).permit(:place_name, :address, :lat, :lng,)
+  # end
 
 end
