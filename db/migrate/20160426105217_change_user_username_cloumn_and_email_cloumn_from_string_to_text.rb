08a0faf10cc8f2de211b2b3cb0ecca79e4031c97
PiHan Hsu <pihanhsu@gmail.com>
2016-05-03 13:37:19 +0800

    remove change_user_username migration

--- db/migrate/20160426105217_change_user_username_cloumn_and_email_cloumn_from_string_to_text.rb
+++
@@ -1,7 +0,0 @@
-class ChangeUserUsernameCloumnAndEmailCloumnFromStringToText < ActiveRecord::Migration
-
-    def change
-      add_column :users, :email, :string
-      add_column :users, :username, :string
-    end
-end
