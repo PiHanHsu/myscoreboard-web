6ce2b704f75a2bdb40c5ba9ee2140f7734e79dab
stevenchentw <sadz9999@hotmail.com>
2016-05-03 22:19:37 +0800

    user can change head form fb or default or paperclip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+
+  def user_avatar(user)
+
+     if user.head?
+       image_url = user.head.url(:thumb)
+     elsif user.fb_pic?
+       image_url = user.fb_pic
+     else
+       image_url = "default_head.png"
+     end
+
+    image_tag image_url
+  end
+
 end
