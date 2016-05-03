7d563d93770f969425b10b9f45988963311541c4
stevenchentw <sadz9999@hotmail.com>
2016-05-04 01:13:29 +0800

    rotate playercard in user show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,19 @@
 module ApplicationHelper
 
 
-  def user_avatar(user)
+def user_avatar(user)
+ if user.head?
+   image_url = user.head.url(:thumb)
+ elsif user.fb_pic?
+   image_url = user.fb_pic
+ else
+   image_url = "default_head.png"
+ end
 
-     if user.head?
-       image_url = user.head.url(:thumb)
-     elsif user.fb_pic?
-       image_url = user.fb_pic
-     else
-       image_url = "default_head.png"
-     end
-
-    image_tag image_url
-  end
+ image_tag image_url
+end
 
 end
+
+
+
