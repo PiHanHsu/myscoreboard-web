a9577c1b7e6754adfcb4e94f8f41e59ff3a72110
PiHan Hsu <pihanhsu@gmail.com>
2016-05-03 13:06:56 +0800

    fix no record crash

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,19 +10,20 @@ class GamesController < ApplicationController
       @team = @teams.first
     end
     
-    @game_type = "mix"
-    if params[:game_type] == "single"
-      @game_type = "single"
-      @male_ranking = @team.male_single_ranking
-      @female_ranking = @team.female_single_ranking
-    elsif params[:game_type] == "double"
-      @game_type = "double"
-      @male_ranking = @team.male_double_ranking
-      @female_ranking = @team.female_double_ranking
-    else
-      @male_ranking = @team.male_mix_ranking
-      @female_ranking = @team.female_mix_ranking
+    if @team
+      @game_type = "mix"
+      if params[:game_type] == "single"
+        @game_type = "single"
+        @male_ranking = @team.male_single_ranking
+        @female_ranking = @team.female_single_ranking
+      elsif params[:game_type] == "double"
+        @game_type = "double"
+        @male_ranking = @team.male_double_ranking
+        @female_ranking = @team.female_double_ranking
+      else
+        @male_ranking = @team.male_mix_ranking
+        @female_ranking = @team.female_mix_ranking
+      end
     end
-
   end
 end
