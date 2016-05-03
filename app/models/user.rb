45f81f948e883123cd984339b2fb4f304bf215b9
stevenchentw <sadz9999@hotmail.com>
2016-05-03 18:00:40 +0800

    gender radio

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   # paperclicp
 
-  has_attached_file :head, styles: { medium: "300x300!", thumb: "100x100!" }, default_url: "/images/:style/missing.png"
+  has_attached_file :head, styles: { medium: "300x300#", thumb: "200x200#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :head, content_type: /\Aimage\/.*\Z/
 
   after_create :set_email_first
