a84b970fea7a79287ead8f44a7394b2851be20ff
PiHan Hsu <pihanhsu@gmail.com>
2016-06-08 20:23:39 +0800

    update FB signup Api

--- app/controllers/api_v1/auth_controller.rb
+++ app/controllers/api_v1/auth_controller.rb
@@ -19,35 +19,30 @@ def signup
     end
 
   elsif params[:access_token]
+    fb_data = User.get_fb_data( params[:access_token] )
+      if fb_data
+        auth_hash = OmniAuth::AuthHash.new({
+          uid: fb_data["id"],
+          info: {
+            email: fb_data["email"],
+            gender: fb_data["gender"],
+            name: fb_data["name"],
+            picture: fb_data["picture"]
+          },
+          credentials: {
+           token: params[:access_token]
+          }
+        })
+        @user = User.from_omniauth_api(auth_hash)
+      end
+        success = fb_data && @user.persisted?
 
-       fb_data = User.get_fb_data( params[:access_token] )
-
-         if fb_data
-           auth_hash = OmniAuth::AuthHash.new({
-             uid: fb_data["id"],
-             info: {
-
-               email: fb_data["email"],
-               gender: fb_data["gender"],
-               name: fb_data["name"],
-               picture: fb_data["picture"]
-
-             },
-             credentials: {
-               token: params[:access_token]
-             }
-           })
-           user = User.from_omniauth_api(auth_hash)
-         end
-       success = fb_data && user.persisted?
-
-         if success
-          render :json => { :auth_token => user.authentication_token,
-                            :user_id => user.id}, :status => 200
-         else
+        if success
+          render :login  # user jbuilder
+        else
           render :json => { :message => "email or password is not correct" }, :status => 401
-         end
-    end
+        end
+  end
 end
 
 # 登入
@@ -64,12 +59,10 @@ end
            auth_hash = OmniAuth::AuthHash.new({
              uid: fb_data["id"],
              info: {
-
                    email: fb_data["email"],
                    gender: fb_data["gender"],
                    name: fb_data["name"],
                    picture: fb_data["picture"]
-
              },
              credentials: {
                token: params[:access_token]
@@ -79,8 +72,6 @@ end
          end
        success = fb_data && user.persisted?
      end
-
-
 
         if success
          # user jbuilder
